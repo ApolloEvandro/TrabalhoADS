a0d90c49e0c59090f3162aaf9555d520d0c38b07
ApolloEvandro <evandroj41@gmail.com>
2025-06-28 04:16:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let botao = document.querySelector("#botao");
+
+    botao.addEventListener("click", function () {
+      botao.innerHTML = "Cada dia é uma nova oportunidade para brilhar.";
+    });
